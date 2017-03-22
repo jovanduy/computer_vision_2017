1df68131b9fe8447defca2700987abbbc4ba8bf0
Kim Asenbeck <kasenbec@wellesley.edu>
2017-03-22 17:53:09 -0400

    sliders are back!

--- cv_parking/scripts/recognizer.py
+++ cv_parking/scripts/recognizer.py
@@ -19,22 +19,59 @@ class ParkingSpotRecognizer(object):
         self.cv_image = None                        # the latest image from the camera
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([0, 70, 60]) # hsv lower bound 
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+        self.hsv_ub = np.array([30, 255, 140]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)        
+
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+        
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+        
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+        
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
+        
                 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        self.binary_image = cv2.inRange(self.hsv_image, (0,70,60), (30,255,140))
+        #self.binary_image = cv2.inRange(self.hsv_image, (0,70,60), (30,255,140))
+        self.binary_image = cv2.inRange(self.hsv_image, self.hsv_lb, self.hsv_ub)
         self.hough_lines()
         
+               
     def hough_lines(self):
        """ This function uses the Hough Line Transform function to identify and visualize lines in our binary image."""
        
-       lines = cv2.HoughLinesP(self.binary_image,1,np.pi/180,100,0,0)
+       lines = cv2.HoughLinesP(self.binary_image, rho=5, theta=np.deg2rad(10), threshold=100, minLineLength=5, maxLineGap=2)
 
-       for x1,y1,x2,y2 in lines[0]:
+       if lines != None:
+           for x1,y1,x2,y2 in lines[0]:
                cv2.line(self.cv_image,(x1,y1),(x2,y2),(0,0,255),2)
    
     def find_endpoints(self):
