9a0c326b660f75c612f8947f28802314577cbf4e
Ziyi Lan <ziyi.lan@students.olin.edu>
2017-03-23 21:35:53 -0400

    now able to determine endpoints for parking spots

--- cv_parking/scripts/recognizer.py
+++ cv_parking/scripts/recognizer.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 from copy import deepcopy
 
+
 class ParkingSpotRecognizer(object):
     """ This robot will recognize a parking spot. """
 
@@ -19,40 +20,10 @@ class ParkingSpotRecognizer(object):
         self.cv_image = None                        # the latest image from the camera
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
-        cv2.namedWindow('threshold_image')
         self.hsv_lb = np.array([0, 70, 60]) # hsv lower bound 
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
-        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
-        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
-        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
         self.hsv_ub = np.array([30, 255, 140]) # hsv upper bound
-        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
-        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
-        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)        
-
-    def set_h_lb(self, val):
-        """ set hue lower bound """
-        self.hsv_lb[0] = val
         
-    def set_s_lb(self, val):
-        """ set saturation lower bound """
-        self.hsv_lb[1] = val
-        
-    def set_v_lb(self, val):
-        """ set value lower bound """
-        self.hsv_lb[2] = val
-
-    def set_h_ub(self, val):
-        """ set hue upper bound """
-        self.hsv_ub[0] = val
-
-    def set_s_ub(self, val):
-        """ set saturation upper bound """
-        self.hsv_ub[1] = val
-        
-    def set_v_ub(self, val):
-        """ set value upper bound """
-        self.hsv_ub[2] = val
         
                 
     def process_image(self, msg):
@@ -62,23 +33,58 @@ class ParkingSpotRecognizer(object):
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
         #self.binary_image = cv2.inRange(self.hsv_image, (0,70,60), (30,255,140))
         self.binary_image = cv2.inRange(self.hsv_image, self.hsv_lb, self.hsv_ub)
-        self.hough_lines()
+        self.spot_delineators = self.find_delineators()
+        if self.spot_delineators != None:
+            cv2.line(self.cv_image,(self.spot_delineators[0][0],self.spot_delineators[0][1]),
+                (self.spot_delineators[0][2],self.spot_delineators[0][3]),(0,255,0),2)
+            cv2.line(self.cv_image,(self.spot_delineators[1][0],self.spot_delineators[1][1]),
+                (self.spot_delineators[1][2],self.spot_delineators[1][3]),(0,255,0),2)
         
                
     def hough_lines(self):
        """ This function uses the Hough Line Transform function to identify and visualize lines in our binary image."""
        
-       lines = cv2.HoughLinesP(self.binary_image, rho=5, theta=np.deg2rad(10), threshold=100, minLineLength=5, maxLineGap=2)
-
+       lines = cv2.HoughLinesP(self.binary_image, rho=5, theta=np.deg2rad(10), threshold=100, minLineLength=25, maxLineGap=0)
+       lines_filtered = []
        if lines != None:
-           for x1,y1,x2,y2 in lines[0]:
-               cv2.line(self.cv_image,(x1,y1),(x2,y2),(0,0,255),2)
+            for x1,y1,x2,y2 in lines[0]:
+                if y1 >100 and y2 > 100 and abs(y1 - y2) > 10:
+                    cv2.line(self.cv_image,(x1,y1),(x2,y2),(0,0,255),2)
+                    lines_filtered.append((x1,y1,x2,y2))
+       return lines_filtered
+
    
-    def find_endpoints(self):
+    def find_delineators(self):
         """This function uses the pinhole camera model to determine the endpoints of a parking spot in 3d space."""
-        pass 
-        
-    
+        lines = self.hough_lines()
+        if len(lines) < 3:
+            return
+        # sorting by left to right in the image
+        lines.sort(key = lambda x: x[0])
+        endpoint1 = lines[0]
+        endpoint2 = -1
+        leftmostx = lines[0][0]
+        x_range = 120
+        index = 1
+        while index < len(lines) and lines[index][0] - leftmostx < x_range :
+            if endpoint1[1] < lines[index][1]:
+                endpoint1 = lines[index]
+            index += 1
+
+        if index < len(lines):
+            endpoint2 = lines[index]
+            leftmostx = endpoint2[0]
+            
+            while index < len(lines) and lines[index][0] - leftmostx < x_range:
+                if endpoint2[1] < lines[index][1]:
+                    endpoint2 = lines[index]
+                index += 1
+
+        if endpoint2 != -1:
+            return [endpoint1, endpoint2]
+        else:
+            return None
+
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
@@ -86,7 +92,6 @@ class ParkingSpotRecognizer(object):
             if not self.cv_image is None:
                 # creates a window and displays the image for X milliseconds
                 cv2.imshow('video_window', self.cv_image)
-                cv2.imshow('hsv_window', self.hsv_image)
                 cv2.imshow('binary', self.binary_image)
                 cv2.waitKey(5)
             r.sleep()
